fc4fb5b24c15d22b8147d0a80d4309ff106a48c6
Tytus Planck <tytusplanck@gmail.com>
2018-07-23 22:55:15 -0400

    Initial commit

---
+++ Rackhouse/Controllers/LoginController.swift
@@ -0,0 +1,106 @@
+//
+//  FirstViewController.swift
+//  Rackhouse
+//
+//  Created by Tytus Planck on 7/23/18.
+//  Copyright © 2018 Tytus Planck. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginController: UIViewController {
+    
+    @IBOutlet weak var fieldLoginEmail: UITextField!
+    @IBOutlet weak var fieldLoginPassword: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Auth.auth().addStateDidChangeListener() { auth, user in
+            if user != nil {
+                self.performSegue(withIdentifier: self.loginToList, sender: nil)
+                self.textFieldLoginEmail.text = nil
+                self.textFieldLoginPassword.text = nil
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginDidTouch(_ sender: AnyObject) {
+        guard
+            let email = fieldLoginEmail.text,
+            let password = fieldLoginPassword.text,
+            email.count > 0,
+            password.count > 0
+            else {
+                return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { user, error in
+            if let error = error, user == nil {
+                let alert = UIAlertController(title: "Sign In Failed",
+                                              message: error.localizedDescription,
+                                              preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    @IBAction func signUpDidTouch(_ sender: AnyObject) {
+        let alert = UIAlertController(title: "Register",
+                                      message: "Register",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            
+            let emailField = alert.textFields![0]
+            let passwordField = alert.textFields![1]
+            
+            Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) { user, error in
+                if error == nil {
+                    Auth.auth().signIn(withEmail: self.textFieldLoginEmail.text!,
+                                       password: self.textFieldLoginPassword.text!)
+                }
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel)
+        
+        alert.addTextField { textEmail in
+            textEmail.placeholder = "Enter your email"
+        }
+        
+        alert.addTextField { textPassword in
+            textPassword.isSecureTextEntry = true
+            textPassword.placeholder = "Enter your password"
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+extension LoginController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == fieldLoginEmail {
+            fieldLoginPassword.becomeFirstResponder()
+        }
+        if textField == fieldLoginPassword {
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+}
+
